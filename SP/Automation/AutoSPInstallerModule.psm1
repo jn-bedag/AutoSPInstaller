976b1b35230fb8477ab2f43249c92309efb37d9d
jn-bedag <34773157+jn-bedag@users.noreply.github.com>
2024-10-24 16:06:56 +0200

    change logfile logtime

--- SP/Automation/AutoSPInstallerModule.psm1
+++ SP/Automation/AutoSPInstallerModule.psm1
@@ -250,9 +250,7 @@ Function StartTracing ($server)
 {
     If (!$isTracing)
     {
-        # Look for an existing log file start time in the registry so we can re-use the same log file
-        $regKey = Get-Item -Path "HKLM:\SOFTWARE\AutoSPInstaller\" -ErrorAction SilentlyContinue
-        If ($regKey) {$script:Logtime = $regkey.GetValue("LogTime")}
+        $script:Logtime = $regkey.GetValue("LogTime")
         If ([string]::IsNullOrEmpty($logtime)) {$script:Logtime = Get-Date -Format yyyy-MM-dd_h-mm}
 
         If ($Global:logPath)
